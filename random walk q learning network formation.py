d6496e0dc25f47d672ca0a31095b8c09d84b0792
haein <appleshi0706@gmail.com>
2021-12-30 10:43:21 +0900

    first commit

---
+++ random walk q learning network formation.py
@@ -0,0 +1,370 @@
+import numpy as np
+import networkx as nx
+from matplotlib import pyplot as plt
+import time
+import math
+
+"""
+basic setup
+1. environment
+- [S 0 1]
+- [2 3 4]
+- [5 6 D]
+- 1x1 block for each agent
+- 3x3 network blocck
+2. state, action, reward
+- state = {number of neighbors} = {0 ~ 8}
+- action = {changing transmission range} = {-1, 0, 1}
+- reward = {throughput improvement} + {panelty for energy consumption}
+- transmission range = {0, 1, 2}
+3. random walk
+- moving distance ~ U(0, 0.01)
+- moving angle ~ U(0, 360)
+"""
+
+max_node = 9  # basic setup : 전체 노드 수를 미리 선언해야 계산 용이
+
+
+# 전체 environment
+class NodeEnv:
+    def __init__(self, txr_level, txr_interval):
+        # envorionment matrix에 해당하는 변수 선언
+        self.net_dim = 3
+        self.one_dim = 1
+        self.max_dim = self.net_dim * self.one_dim
+        self.net_block = self.net_dim ** 2  # 정사각형 environment 가정
+        self.num_node = self.net_block
+
+        # default block coordination 선언
+        self.block_coords = np.zeros((self.net_block, 2))
+        i = 0
+        for x in range(0, self.max_dim, self.one_dim):
+            for y in range(0, self.max_dim, self.one_dim):
+                self.block_coords[i, :] = [x, y]
+                i += 1
+
+        self.net_block_coords = self.block_coords.tolist()
+
+        # agent location 지정
+        self.node_loc = []
+        for i in range(self.num_node):
+            self.node_loc.append(self.net_block_coords[i])
+
+        # source, destination 제외
+        del self.node_loc[0]
+        del self.node_loc[self.num_node - 2]
+
+        # tx range, action 변수 선언
+        self.txr_level = txr_level # 1
+        self.txr_interval = txr_interval # 1
+        self.max_txr = self.txr_level * self.txr_interval # max_txr=1
+        self.action_space = np.arange(-self.max_txr, self.max_txr + 1, self.txr_interval) # (-1, 2, 1)
+        self.num_action = len(self.action_space)
+
+    @staticmethod
+    def DistM(node_loc):  # distance matrix 계산
+        dist_M = np.zeros([max_node, max_node])
+        for i in range(max_node):
+            for j in range(max_node):
+                dist_M[i, j] = np.linalg.norm(np.array(node_loc[i]) - np.array(node_loc[j]))
+        return dist_M
+
+    @staticmethod  # adjacency matrix 계산
+    def AdjM(dist_M, tx_range):
+        state_adj_M = np.zeros([max_node, max_node])
+
+        for i in range(max_node):
+            state_adj_M[i] = ((dist_M[i]) <= tx_range[i])
+
+        return state_adj_M
+
+    @staticmethod
+    def get_State(state_adj_M):  # node 별로 state 계산 (자기자신은 제외)
+        sum_adj_M = np.zeros([max_node])
+        sum_adj_M = state_adj_M.sum(axis=1)
+        next_state = np.zeros([max_node])
+
+        for i in range(max_node):
+            next_state[i] = sum_adj_M[i] - 1
+
+        return next_state
+
+    @staticmethod
+    def get_Throughput(adj_M, dist_M):  # network throughput 계산
+        dest = max_node - 1
+        source = 0
+
+        net_G = nx.DiGraph()
+        for i in range(max_node):
+            net_G.add_node(i)
+            for j in range(max_node):
+                if adj_M[i, j] == 1:
+                    # weight 적어주기
+                    net_G.add_weighted_edges_from([(i, j, dist_M[i, j])])
+
+        if nx.has_path(net_G, source, dest):
+            #최단경로
+            path_txr = nx.shortest_path_length(net_G, source, dest)
+
+        else:
+            path_txr = np.inf
+
+        throughput = 10 / path_txr  # scaling factor로 source to destination 거리 나누기
+
+        return throughput
+
+    def takeStep(self, action, next_txr, last_throughput, node_loc):  # action 수행 결과 도출
+        node_loc = node_loc.tolist()
+        # 노드 movement 상황
+        node_loc.insert(0, [0.0, 0.0])  # source location 추가
+        node_loc.append([2.0, 2.0])  # destination location 추가
+
+        next_txr = next_txr.tolist()
+        next_txr.insert(0, 1.5)  # 리스트 내 index =0인 자리에 1.5 추가
+        next_txr.append(0.0)  # 리스트 내 index 마지막 자리에 0 추가
+
+        next_dist_M = self.DistM(node_loc)
+        next_adj_M = self.AdjM(next_dist_M, next_txr)
+
+        next_state = self.get_State(next_adj_M)
+        cur_throughput = self.get_Throughput(next_adj_M, next_dist_M)
+
+        # get reward
+        pos_coeff = 1
+        balance_w = 0.45  # 0.45: throughput 보장하면서 action 최적화하는 최솟값 discount factor
+        net_reward = np.zeros([max_node - 2])
+
+        for i in range(max_node - 2):
+            net_reward[i] = pos_coeff + balance_w * (cur_throughput - last_throughput) - (1 - balance_w) * action[i]
+
+        node_loc.remove([0.0, 0.0])
+        node_loc.remove([2.0, 2.0])
+        node_loc = np.array(node_loc)
+
+        return next_state, net_reward, cur_throughput
+
+    # episode 시작할 때마다 environment reset
+    def reset(self):
+        now_txr = np.zeros([max_node - 2])
+        current_state = np.zeros([max_node - 2])
+        cur_loc = self.node_loc
+        last_throughput = 0
+
+        return current_state, now_txr, cur_loc, self.node_loc, last_throughput
+
+
+class QlearningNode:
+    def __init__(self, action_space, q_table):
+        self.learningrate = 1e-3
+        self.discount_factor = 0.9
+        self.action_space = action_space
+        self.num_action = len(self.action_space)
+
+        self.q_table = q_table
+
+        # 무빙 에이전트에 해당 하는 변수 선언
+        self.node_net_size = 1  # block 밖으로 나가지 못하게 제한하기 위함
+
+    def print_qtable(self):
+        print(self.q_table)
+
+    def learn(self, epoch, state, action, reward, next_state):
+        state = int(state)
+        action_space = self.action_space.tolist()
+        action_idx = action_space.index(action)
+        current_q = self.q_table[state, action_idx]
+        next_q = reward + self.discount_factor * np.max(self.q_table[int(next_state), :])
+        self.q_table[state, action_idx] += self.learningrate * (next_q - current_q)
+
+    def getAction(self, cur_state, eps, cur_txr):
+        if (eps > np.random.rand()):  # uniform random action selection
+            action_idx = np.random.randint(0, 3)
+            action = self.action_space[int(action_idx)]
+            next_txr = cur_txr + action
+        else:
+            state_action = self.q_table[int(cur_state), :]
+            action_idx = np.argmax(state_action)
+            action = self.action_space[int(action_idx)]
+            next_txr = cur_txr + action
+
+        # 정해진 tx range 범위 내에서만 action 고르도록...?
+            action_idx = np.random.randint(0, 3)
+            action = self.action_space[int(action_idx)]
+            next_txr = cur_txr + action
+
+        return action, action_idx, next_txr
+
+    # movement part
+    def RandomWalk(self, original_node_loc, now_node_loc):
+        random_direction = np.random.randint(0, 360)
+        moving_distance = np.random.rand() * 0.01
+
+        if random_direction == 360:
+            next_node_loc = now_node_loc
+
+        else:
+            next_node_loc = [moving_distance * math.cos((random_direction)) + now_node_loc[0],
+                             moving_distance * math.sin((random_direction)) + now_node_loc[1]]
+
+        # 할당된 block 벗어나지 않도록 x, y 좌표 확인
+        row = next_node_loc[0]
+        col = next_node_loc[1]
+
+        ori_row = original_node_loc[0]
+        ori_col = original_node_loc[1]
+
+        lim_row = self.node_net_size + ori_row
+        lim_col = self.node_net_size + ori_col
+
+        if (row >= ori_row) and (row <= lim_row) and (col >= ori_col) and (col <= lim_col):
+            return next_node_loc
+
+        else:  # block 벗어나면 다시 뽑기
+            try:
+                return self.RandomWalk(original_node_loc, now_node_loc)
+            except RecursionError:
+                return now_node_loc
+
+
+if __name__ == '__main__':
+
+    tic = time.time()
+
+    txr_level = 1  # tx range 총 단계
+    txr_interval = 1  # tx range 간격
+
+    env = NodeEnv(txr_level, txr_interval)
+    action_space = env.action_space
+
+    episode = 0  # 1 episode = 100 step
+    num_episodes = 20000
+    num_step = 100
+
+    # epsilon greedy method
+    eps_end = 0.01
+    eps_start = 1.0
+    eps_decay = 1e-4
+    eps_trace = []
+
+    # trace method
+    epi_reward = 0
+    last_reward = 0
+    reward = []
+    epi_through = 0
+    throughput_trace = []
+    reward_trace = []
+    txr_step = np.zeros([num_step, max_node - 2])  # 스텝별 tx range 모두 저장
+    txr_node = np.zeros([num_episodes, max_node - 2])  # episode의 평균 tx range
+
+    next_loc = np.zeros([max_node - 2, 2])  # 노드의 x, y축 좌표
+
+    q_table = np.zeros([max_node - 2, max_node, len(action_space)])  # (총 agent 수, state, action space)
+
+    node = []
+    for i in range(max_node - 2):  # for all agents
+        node.append(QlearningNode(action_space, q_table[i, :, :]))
+
+    for epi in range(num_episodes):
+
+        state, cur_txr, cur_node_loc, ori_node_loc, last_throughput = env.reset()
+        cur_node_loc = np.array(cur_node_loc)
+        ori_node_loc = np.array(ori_node_loc)
+
+        # epsilon greedy : episode 진행에 따라 explore 비율 감소
+        eps = max(eps_end, eps_start - (eps_decay * epi))
+        eps_trace.append(eps)
+
+        # trace method
+        reward_trace.append(epi_reward / num_step)
+        throughput_trace.append(epi_through / num_step)
+        txr_node[epi, :] = np.round(txr_step.sum(axis=0) / num_step)
+        txr_step = np.zeros([num_episodes, max_node - 2])
+        epi_reward = 0
+        epi_through = 0
+
+        for step in range(num_step):
+            # initializing
+            action_idx = np.zeros([max_node - 2])
+            next_txr = np.zeros([max_node - 2])
+            action = np.zeros([max_node - 2])
+
+            # epsilon greedy policy에 의해 action select
+            for i in range(max_node - 2):
+                action[i], action_idx[i], next_txr[i] = node[i].getAction(state[i], eps, cur_txr[i])
+
+            next_state, reward, goodput = env.takeStep(action, next_txr, last_throughput, cur_node_loc)
+
+            # source, destination은 state 개념 없으므로 삭제
+            next_state = np.delete(next_state, max_node - 1)
+            next_state = np.delete(next_state, 0)
+
+            for i in range(max_node - 2):
+                node[i].learn(epi, state[i], action[i], reward[i], next_state[i])
+                next_loc[i] = node[i].RandomWalk(ori_node_loc[i], cur_node_loc[i])  # random walk model
+
+            state = next_state
+            cur_txr = next_txr
+            last_throughput = goodput
+            cur_node_loc = next_loc
+            epi_reward += (sum(reward) / (max_node - 2))  # 모든 에이전트들의 평균 reward
+            epi_through += last_throughput
+            txr_step[step, :] = next_txr
+
+    # print every result
+    toc = time.time()
+    print("processing time", toc - tic)
+
+    print("\n")
+    print("eps ", eps)
+
+    for i in range(max_node - 2):
+        print("%s node" % i)
+        node[i].print_qtable()
+
+    # 전체 에피소드에 대해 50개 단위로 나누어 평균 취해서 그래프 plot
+    reward_trace_sum = []
+    for i in range(0, num_episodes, 50):
+        reward_trace_sum.append(sum(reward_trace[i:i + 50]) / 50)
+    throughput_trace_sum = []
+    for i in range(0, num_episodes, 50):
+        throughput_trace_sum.append(sum(throughput_trace[i:i + 50]) / 50)
+    reward_trace.pop(0)
+    throughput_trace.pop(0)
+    reward_trace_sum.pop(0)
+    throughput_trace_sum.pop(0)
+
+    plt.figure()
+    plt.xlabel('Episodes')
+    plt.ylabel('Reward')
+    plt.plot(reward_trace)
+
+    plt.figure()
+    plt.xlabel('Episodes')
+    plt.ylabel('Average Reward')
+    plt.plot(reward_trace_sum)
+
+    plt.figure()
+    plt.xlabel('episode')
+    plt.ylabel('epsilon')
+    plt.title('epsilon')
+    plt.plot(eps_trace)
+
+    plt.figure()
+    plt.xlabel('Episodes')
+    plt.ylabel('Throughput')
+    plt.plot(throughput_trace)
+
+    plt.figure()
+    plt.xlabel('Episodes')
+    plt.ylabel('Throughput')
+    plt.plot(throughput_trace_sum)
+
+    plt.figure()
+    plt.xlabel('episode')
+    plt.ylabel('tx range')
+    plt.title('transmission range')
+    for i in range(max_node - 2):
+        plt.plot(txr_node[1:, i], label='%s node' % i)
+        plt.legend()
+
+    plt.show()
