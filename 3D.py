905c66676373c4f49c9835da799c5a9980d3ac96
haein <appleshi0706@gmail.com>
2022-01-18 14:28:04 +0900

    그래프에 sphere형태로 나타냄

--- 3D.py
+++ 3D.py
@@ -16,7 +16,9 @@ observation :
 action : move the node, change the transmission radius
 reward : throughput + 수선의 발 길이의 차 - node 이동거리 - transmission radius 유지를 위한 에너지량
 """
-total_node = 4 #source, n1, n2, destination
+
+#source, n1, n2, destination
+total_node = 4
 
 class setup :
     def __init__(self, source, destination) :
@@ -25,17 +27,19 @@ class setup :
         self.array[1,:] = destination[:]
         """for i in range (0,total_node-2,1) :
             self.array[i+2,:]= destination[:]"""
-        self.array[2,:] = [4,4,4,2]
-        self.array[3,:] = [3,3,3,2]
+        self.array[2,:] = [4,4,4,3]
+        self.array[3,:] = [3,3,3,3]
 
+    #action 수행하기
     def do_action(self, i, move):
         next_array = self.array.copy()
-        next_array[i][0] = self.array[i][0] + move[0]
-        next_array[i][1] = self.array[i][1] + move[1]
-        next_array[i][2] = self.array[i][2] + move[2]
-        next_array[i][3] = self.array[i][3] + move[3]
+        next_array[i][0] = self.array[i][0] + move[0] #x축
+        next_array[i][1] = self.array[i][1] + move[1] #y축
+        next_array[i][2] = self.array[i][2] + move[2] #z축
+        next_array[i][3] = self.array[i][3] + move[3] #txr
         return next_array
 
+    #S-D까지 연결되는지 확인하기위해서 인접행렬 만들기
     def cal_adjacency(self, next_array):
         adj_array = np.empty((total_node,total_node),bool)
         for i in range (0, total_node, 1):
@@ -47,9 +51,10 @@ class setup :
                     adj_array[i][j] = True
                 else :
                     adj_array[i][j] = False
-
         return adj_array
 
+    #인접그래프 그리기.
+    #S-D까지의 경로가 있나 확인 -> 홉의 개수(has_path최단거리)찾기 -> throughput 구하기
     def cal_throughput(self, adj_array):
         graph = nx.Graph()
         for i in range(0,total_node,1):
@@ -73,6 +78,7 @@ class setup :
 
         return throughput
 
+    #(시간t일때의 수선의 발 - 시간t+1일때 수선의 발)길이 구하기
     def cal_foot_of_perpendicular(self, next_array, i, move):
         foot_of_perpendicular = math.sqrt(math.pow(((-2*(self.array[i][0] - self.array[0][0])) + (self.array[i][1] - self.array[0][1]) + (self.array[i][2] - self.array[0][2]))/3,2)
                                           +math.pow((((self.array[i][0] - self.array[0][0])) + (-2)*(self.array[i][1] - self.array[0][1]) + (self.array[i][2] - self.array[0][2]))/3,2)
@@ -96,16 +102,21 @@ class setup :
         reward = 7 + throughput + foot_of_perpendicular - energy_move - energy_txr
         return reward
 
+    #에피소드마다 node들의 위치 리셋시키기
     def reset(self,destination):
         for i in range (0,total_node-2,1) :
             self.array[i+2,:]= destination[:]
 
+
+
+#메인함수
 if __name__ == '__main__':
 
-    source = np.array([0,0,0,2])
-    destination = np.array([4,4,4,2])
+    source = np.array([0,0,0,3])
+    destination = np.array([4,4,4,0])
+
     i = 2 #action
-    move = np.array([-1,-1,0,0]) #action
+    move = np.array([-2,-3,-3,0]) #action
 
     env = setup(source, destination)
     next_arr = env.do_action(i, move)
@@ -115,40 +126,63 @@ if __name__ == '__main__':
     e_move = env.cal_used_energy_to_move(move)
     e_txr = env.cal_used_energy_to_keep_txr(i,next_arr)
     reward = env.cal_reward(throughput,foot,e_move,e_txr)
-    #print("throughput : ",throughput)
+
+    print("move : ",move)
+    print("throughput : ",throughput)
     #print("foot : " ,foot)
     #print("e_move : " ,e_move)
     #print("e_txr : " ,e_txr)
     print("reward : " ,reward)
+    print("array :\n",env.array)
     print("next_arr :\n",next_arr)
-    #print("array :\n",env.array)
 
-    scatter_array = np.empty((3,total_node),int)
-    for i in range(0, 3,1):
+
+    #node들의 좌표 배열 구하기
+    scatter_array = np.empty((4,total_node),int)
+    for i in range(0, 4,1):
         for j in range(0,total_node,1) :
             scatter_array[i][j]=next_arr[j][i]
-    print("scatter_array :\n",scatter_array)
+    #print("scatter_array :\n",scatter_array)
 
 
+    def create_sphere(cx, cy, cz, r):
+
+        u, v = np.mgrid[0:2 * np.pi:20j, 0:np.pi:10j]
+
+        x = np.cos(u) * np.sin(v)
+        y = np.sin(u) * np.sin(v)
+        z = np.cos(v)
+        # shift and scale sphere
+        x = r * x + cx
+        y = r * y + cy
+        z = r * z + cz
+        return (x, y, z)
+
+    #3D 그래프 그리기
     fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
+    ax = fig.add_subplot(111, projection='3d')
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
     ax.set_xlim3d(0, 4)
     ax.set_ylim3d(4, 0)
     ax.set_zlim3d(0, 4)
+    ax.set_xticks([0,1,2,3,4])
+    ax.set_yticks([0,1,2,3,4])
+    ax.set_zticks([4,3,2,1,0])
     # scatter() 함수에 준비된 x, y, z 배열 값을 입력
     # marker = 점의 형태
     # s = 점의 크기
     # c = 점의 색깔
-    ax.scatter(scatter_array[0],scatter_array[1],scatter_array[2], marker='o', s=15, c='darkgreen')
+    # draw sphere
+    sphere_array = np.empty((3, total_node), int)
+    for i in range(0,total_node,1):
+        (x,y,z) = create_sphere(next_arr[i][0], next_arr[i][1],next_arr[i][2],next_arr[i][3])
+        ax.plot_surface(x,y,z,color="r",linewidth=0,alpha=0.2)
     #ax.view_init(40,30) #각도 지정
+        ax.scatter(scatter_array[0],scatter_array[1],scatter_array[2], marker='o', s=15, c='darkgreen')
+
 
     plt.show()
+
     env.reset(destination)
-
-
-
-
-
