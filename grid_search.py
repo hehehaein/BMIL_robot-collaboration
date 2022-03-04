ad3e0fb489b88f97c2170d0e5c457e0ebb65e249
haein <appleshi0706@gmail.com>
2022-03-04 20:11:27 +0900

    grid_search.py 추가5

--- grid_search.py
+++ grid_search.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 N=2
 # ~transmission radius max
-R_MAX = 4
+R_MAX = 3
 # location x,y,z
 MIN_LOC = 0
 MAX_LOC = 4
@@ -21,17 +21,25 @@ state[1]=np.array((3, 3, 3, 3))
 state[2]=np.array((MIN_LOC, MIN_LOC, MIN_LOC, 3))
 state[3]=np.array((MAX_LOC, MAX_LOC, MAX_LOC, 0))
 
-'''agent = np.zeros((6,4))
+agent = np.zeros((6,4))
 agent[0] = (1,1,2,3)
 agent[1] = (1,2,1,3)
 agent[2] = (2,1,1,3)
 agent[3] = (2,2,1,3)
 agent[4] = (2,1,2,3)
-agent[5] = (1,2,2,3)'''
+agent[5] = (1,2,2,3)
 
 env = reward_set(N)
+for i in range(0,6,1):
+    state[0] = agent[i]
+    adj_array = env.cal_adjacency(state)
+    throughput = env.cal_throughput(adj_array)
+    foot = env.cal_foot(state, source, dest, 0)
+    dispersed = env.cal_dispersed(0, state[0][3], adj_array)
+    reward = throughput+foot+dispersed
+    print(state[0], 'throughput=',throughput,'foot=',foot,'dispersed=',dispersed, 'reward=', reward)
 
-fig = plt.figure()
+'''fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
@@ -44,11 +52,13 @@ xs = []
 ys = []
 zs = []
 alphas = []
+max=0
+max_location = np.zeros((1,4))
 color_list = ("bisque", "navajowhite", "orange", "darkorange", "darkgoldenrod", "olive", "olivedrab","darkgreen","darklategray","blue")
 for x in range(0,5,1):
     for y in range(0,5,1):
         for z in range(0,5,1):
-            for r in range(0,5,1):
+            for r in range(0,4,1):
                 xs.append(x)
                 ys.append(y)
                 zs.append(z)
@@ -60,8 +70,12 @@ for x in range(0,5,1):
                 dispersed = env.cal_dispersed(0, state[0][3], adj_array)
                 reward = throughput+foot+dispersed
                 print(state[0], 'throughput=',throughput,'foot=',foot,'dispersed=',dispersed, 'reward=', reward)
-                reward_save = (reward+5)/11
+                reward_save = (reward+5)/40
                 alphas.append(reward_save)
-ax.scatter(xs,ys,zs,marker='o', s=100, c='orange', alpha=alphas)
-plt.show()
+                if max < reward :
+                    max = reward
+                    max_location = np.array((x,y,z,r))
+print(max, max_location)
+ax.scatter(xs,ys,zs,marker='o', s=100, c='black', alpha=alphas)
+plt.show()'''
 
